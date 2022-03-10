540deea82e4c24084ae5784c6c0eee5ee16ea1b1
Sheldon <sheldonaldridge@yahoo.com>
2022-03-10 13:11:24 +0200

    Create javscript.js

---
+++ js/javscript.js
@@ -0,0 +1,41 @@
+
+function redColour()
+{
+    document.getElementById("red").style.backgroundColor ="red";
+    document.getElementById("red").innerHTML="red";
+}
+
+function orangeColour()
+{
+    document.getElementById("orange").style.backgroundColor ="orange";
+    document.getElementById("orange").innerHTML="orange";
+}
+
+function yellowColour()
+{
+    document.getElementById("yellow").style.backgroundColor ="yellow";
+    document.getElementById("yellow").innerHTML="yellow";
+}
+
+function greenColour()
+{
+    document.getElementById("green").style.backgroundColor ="green";
+    document.getElementById("green").innerHTML="green";
+}
+
+function blueColour()
+{
+    document.getElementById("blue").style.backgroundColor ="blue";
+    document.getElementById("blue").innerHTML="blue";
+}
+function purpleColour()
+{
+    document.getElementById("purple").style.backgroundColor ="purple";
+    document.getElementById("purple").innerHTML="purple";
+}
+
+function lavenderColour()
+{
+    document.getElementById("lavender").style.backgroundColor ="#967bb6";
+    document.getElementById("lavender").innerHTML="lavender";
+}
